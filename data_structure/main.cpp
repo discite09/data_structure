42f57a34da3ae9aaf1814b9b198aefddd37299a6
Discite <disciteeticsid09@gmail.com>
2025-10-17 09:15:22 +0900

    Initial Commit

---
+++ data_structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_structure
+//
+//  Created by ZeroNine Discite on 10/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
